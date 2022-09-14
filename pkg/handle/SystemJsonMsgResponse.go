9b2f11af60449b2e6ec88fbf2f61b3d70913ab8c
Spark <934913797@qq.com>
2022-09-14 18:23:47 +0800

    feat: 统一http请求返回

---
+++ pkg/handle/SystemJsonMsgResponse.go
@@ -0,0 +1,44 @@
+package handle
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SystemJsonMsgResponse struct {
+	Ctx *gin.Context
+}
+
+type JsonMsgResult struct {
+	Code    int
+	Message string
+	Data    interface{}
+}
+
+const SUCCESS_CODE = 200
+const SUCCESS_MSG = "成功"
+const ERROR_MSG = "错误"
+
+func NewResponse(c *gin.Context) *SystemJsonMsgResponse {
+	return &SystemJsonMsgResponse{Ctx: c}
+}
+
+func (r *SystemJsonMsgResponse) Success(data interface{}) {
+	res := JsonMsgResult{}
+	res.Code = SUCCESS_CODE
+	res.Message = SUCCESS_MSG
+	res.Data = data
+	r.Ctx.JSON(http.StatusOK, res)
+}
+
+func (r *SystemJsonMsgResponse) Error(code int, message string) {
+	if message == "" {
+		message = ERROR_MSG
+	}
+	res := JsonMsgResult{}
+	res.Code = code
+	res.Message = message
+	res.Data = gin.H{}
+	r.Ctx.JSON(http.StatusOK, res)
+}
