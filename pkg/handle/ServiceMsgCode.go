d8ac84c0953989fdde0f7dfaad37eedc036caf6b
Spark <934913797@qq.com>
2022-09-16 14:53:54 +0800

    docs: 文件更名，并添加了一些包(无实际意义)

--- pkg/handle/ServiceMsgCode.go
+++ pkg/handle/ServiceMsgCode.go
@@ -10,20 +10,28 @@ var (
 	SUCCESS = MsgCode{Code: 200, Msg: "成功"}
 
 	/* 默认失败 */
-	COMMON_FAIL = MsgCode{-1, "失败"}
+	COMMON_FAIL = MsgCode{-4396, "失败"}
 
-	/* 参数错误：1000～1999 */
+	/* 请求错误 <0 */
+	TOKEN_IS_EXPIRED = MsgCode{-2, "token已过期"}
+
+	/* 内部错误 600 ~ 999*/
+	INTERNAL_ERROR = MsgCode{601, "内部错误, check log"}
+
+	/* 参数错误：1000 ~ 1999 */
 	PARAM_NOT_VALID    = MsgCode{1001, "参数无效"}
 	PARAM_IS_BLANK     = MsgCode{1002, "参数为空"}
 	PARAM_TYPE_ERROR   = MsgCode{1003, "参数类型错误"}
 	PARAM_NOT_COMPLETE = MsgCode{1004, "参数缺失"}
 
-	/* 用户错误 */
-	USER_NOT_LOGIN = MsgCode{2001, "用户未登录"}
+	/* 用户错误 2000 ~ 2999*/
+	USER_NOT_LOGIN          = MsgCode{2001, "用户未登录"}
+	USER_PASSWORD_DIFFERENT = MsgCode{2002, "用户两次密码输入不一致"}
+	USER_CREDENTIALS_ERROR  = MsgCode{2003, "密码错误"}
 	/*
 	 USER_NOT_LOGIN(2001, "用户未登录"),
 	 USER_ACCOUNT_EXPIRED(2002, "账号已过期"),
-	 USER_CREDENTIALS_ERROR(2003, "密码错误"),
+
 	 USER_CREDENTIALS_EXPIRED(2004, "密码过期"),
 	 USER_ACCOUNT_DISABLE(2005, "账号不可用"),
 	 USER_ACCOUNT_LOCKED(2006, "账号被锁定"),
