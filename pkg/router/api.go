dbd8c1b28139c07ee621b4a899511322ff51c5a7
Jett <jetttcc@gmail.com>
2024-03-29 02:32:43 +0800

    feat(hertz): handle router

--- pkg/router/api.go
+++ pkg/router/api.go
@@ -4,15 +4,15 @@ import (
 	"context"
 
 	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/common/utils"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/cloudwego/hertz/pkg/route"
 )
 
 func init() {
-	RegisterRouter(func(h *server.Hertz) {
-		h.GET("/unverity_users", Test)
-	})
+	RegisterRouter(func(r *route.RouterGroup) {
+		r.GET("/test", Test)
+	}, V1Router)
 }
 
 func Test(ctx context.Context, c *app.RequestContext) {
--- pkg/router/web.go
+++ pkg/router/web.go
@@ -2,22 +2,37 @@ package router
 
 import (
 	"cloud-platform/global"
-	"context"
 	"fmt"
 
-	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/route"
 )
 
-type Hook func(ctx context.Context, c *app.RequestContext)
-type Router func(h *server.Hertz)
+type Router func(h *route.RouterGroup)
+
+type RouteLevel struct {
+	Level int16
+	Url   string
+}
 
 var (
-	routers []Router
+	V1Router = RouteLevel{1, "/api/v1"}
+	V2Router = RouteLevel{2, "/api/v2"}
+	V3Router = RouteLevel{3, "/api/v3"}
 )
 
-func RegisterRouter(router Router) {
-	routers = append(routers, router)
+var (
+	routersSize = 0
+	mapRouters  = map[RouteLevel][]Router{
+		V1Router: make([]Router, 0),
+		V2Router: make([]Router, 0),
+		V3Router: make([]Router, 0),
+	}
+)
+
+func RegisterRouter(router Router, level RouteLevel) {
+	routersSize++
+	mapRouters[level] = append(mapRouters[level], router)
 }
 
 func RunS() {
@@ -31,8 +46,16 @@ func RunS() {
 
 func Listen() (*server.Hertz, error) {
 	h := server.Default(server.WithHostPorts(fmt.Sprintf("%s:%d", global.Config.App.Host, global.Config.App.Port)))
-	for _, router := range routers {
-		router(h)
-	}
+	registerRouter(h)
 	return h, nil
+}
+
+func registerRouter(h *server.Hertz) {
+	global.Logger.Infof("register router start, size: %d", routersSize)
+	for level, routers := range mapRouters {
+		v := h.Group(level.Url)
+		for _, router := range routers {
+			router(v)
+		}
+	}
 }
